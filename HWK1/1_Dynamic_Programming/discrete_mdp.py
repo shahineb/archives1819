0c340e6914be3b585060b54e2bad6c9eabe1f5b6
shahine <shahine96@gmail.com>
2018-10-31 18:39:58 +0100

    hmwk1-part1 completed

--- HWK1/1_Dynamic_Programming/discrete_mdp.py
+++ HWK1/1_Dynamic_Programming/discrete_mdp.py
@@ -3,7 +3,7 @@ from utils import *
 
 class DiscreteMDP(object):
 
-    def __init__(self, states_names, action_names, transition_proba, reward, policy, gamma=0.95):
+    def __init__(self, states_names, action_names, transition_proba, reward, optimal_policy=None, gamma=0.95):
         """
 
         Parameters
@@ -19,8 +19,9 @@ class DiscreteMDP(object):
         reward : numpy.array
             (n_states, n_actions_) numpy.array providing the reward
             for each action taken from a given state
-        policy : list[int]
-            len = n_states_, actions to be taken in each state
+        optimal_policy : list[int]
+            len = n_states_, actions to be taken in each state wrt
+            guessed optimal policy
         gamma : float
             discount factor
         """
@@ -33,14 +34,16 @@ class DiscreteMDP(object):
 
         self.transition_proba_ = transition_proba
         self.reward_ = reward
-        self.policy_ = policy
-
+        self.optimal_policy_ = optimal_policy
         self.gamma_ = gamma
-        self.value_ = np.random.randn(self.n_states_)
+
+        self.value_ = None
+        self.policy_ = None
+        self.iteration_ = None
 
 
 
-    def reset(self):
+    def reset(self, initial_policy=None):
         """
         Returns:
         ---------
@@ -48,56 +51,27 @@ class DiscreteMDP(object):
         """
         initial_state = np.random.choice(np.arange(self.n_states_))
         self.value_ = np.random.randn(self.n_states_)
+        self.iteration_ = 0
+        if initial_policy:
+            self.policy_ = initial_policy
+        else:
+            self.policy_ = np.random.choice(np.arange(self.n_actions_), size=self.n_states_)
         return initial_state
 
 
-    def step(self, state, action):
+
+
+    def compute_optimal_policy(self):
+        """returns policy maximizing state value function
         """
-        Parameters
-        ----------
-        state : int
-            index of current state
-        action : int
-            index of taken action
-
-        Returns
-        -------
-        next_state : ini
-            index of next state
-        step_reward : float
-            reward for step move
-        """
-
-        self.iteration_ += 1
-
-        p_dist = self.transition_proba_[state, action]
-        next_state = np.random.choice(np.arange(self.n_states_), p=p_dist)
-
-        step_reward = self.reward_[state, action]
-        # self.value_ += (self.gamma_**self.iteration_)*step_reward
-
-        return next_state
-
-
-
-    def next_action(self, state):
-        """Returns action to take from a given state wrt policy
-
-        Parameters
-        ----------
-        state : int
-
-        Returns
-        -------
-        int
-            action
-        """
-        return self.policy_[state]
+        foo = self.reward_ + self.gamma_*np.sum(self.transition_proba_*self.value_, axis=2)
+        optimal_policy = np.argmax(foo, axis=1)
+        return optimal_policy
 
 
     def update_state_value(self):
         """Updates state value function by applying to it the
-        optimal Bellman operator
+        optimal Bellman operator for value iteration
         """
         self.value_ = bellman_operator(self.reward_,
                                        self.transition_proba_,
@@ -105,12 +79,24 @@ class DiscreteMDP(object):
                                        self.gamma_)
 
 
-    def get_optimal_policy(self):
-        """returns policy maximizing state value function
+    def update_policy(self):
+        """Updates state value function and policy using direct computation
+        for oplicy iteration
         """
-        foo = self.reward_ + self.gamma_*np.sum(self.transition_proba_*self.value_, axis=2)
-        optimal_policy = np.argmax(foo, axis=1)
-        return optimal_policy
+        # get indexes wrt policy to retrieve appropriate reward and
+        # transition matrix
+        indexes = [np.arange(self.n_states_), self.policy_]
+
+        reward = self.reward_[indexes]
+        transition_proba = self.transition_proba_[indexes]
+
+        # Compute updated value
+        foo = np.eye(self.n_states_)-self.gamma_*transition_proba
+        self.value_ = np.matmul(np.linalg.inv(foo), reward)
+
+        # Compute new policy
+        self.policy_ = self.compute_optimal_policy()
+
 
 
     def policy_evaluation(self, policy):
@@ -124,28 +110,84 @@ class DiscreteMDP(object):
         return value
 
 
-    def run_value_iteration(self, eps, max_iter=10000, track=False):
+    def run_value_iteration(self, eps=0.01, max_iter=10000, track=False):
+        """Runs value iteration
+
+        Parameters
+        ----------
+        eps : float
+            Threshold of stopping criterion
+        max_iter : int
+            maximum number of iterations allowed in case convergence is not
+            reached
+        track : boolean
+            if True, keeps record of state values over iteration in order
+            to compute uniform distance between optimal value at each step
+
+        Returns
+        -------
+        optimal_policy : list[int]
+            optimal policy reached
+
+        distance_to_optimal : list[float]
+            distances between guessed optimal value and value computed at
+            each iteration
+
+        n_iter : int
+        """
         state = self.reset()
 
-        conv_criteria = True
+        conv_criterion = True
         n_iter = 0
 
         if track:
-            distance = []
-            optimal_value = self.policy_evaluation(self.policy_)
+            values_history = []
 
-        while conv_criteria and n_iter < max_iter:
+        while conv_criterion and n_iter < max_iter:
             n_iter += 1
-            conv_criteria = self.value_
+            conv_criterion = self.value_
 
             self.update_state_value()
-            conv_criteria = np.linalg.norm(conv_criteria-self.value_, ord=np.inf) > eps
+            self.iteration_ += 1
+            conv_criterion = np.linalg.norm(conv_criterion-self.value_, ord=np.inf) > eps
 
             if track:
-                distance += [np.linalg.norm(self.value_-optimal_value, ord=np.inf)]
+                values_history += [self.value_]
 
-        optimal_policy = self.get_optimal_policy()
+        optimal_policy = self.compute_optimal_policy() # optimal policy obtained by VI
+
         if track :
-            return optimal_policy, distance, n_iter
+            optimal_value = self.policy_evaluation(self.optimal_policy_) # optimal value wrt to guessed optimal policy
+            distance_to_optimal = [np.linalg.norm(x-optimal_value, ord=np.inf) for x in values_history]
+            return optimal_policy, distance_to_optimal, n_iter
         else:
             return optimal_policy
+
+
+
+
+    def run_policy_iteration(self, initial_policy=None, eps=0.01, max_iter=10000):
+        """Runs policy iteration
+
+        Parameters
+        ----------
+        eps : float
+            Threshold of stopping criterion
+        max_iter : int
+            maximum number of iterations allowed in case convergence is not
+            reached
+        """
+
+        state = self.reset(initial_policy=initial_policy)
+
+        conv_criterion = True
+        n_iter = 0
+
+        while conv_criterion and n_iter < max_iter :
+            n_iter += 1
+            conv_criterion = self.value_
+
+            self.update_policy()
+            self.iteration_ += 1
+            conv_criterion = np.linalg.norm(conv_criterion-self.value_, ord=np.inf) > eps
+        return self.policy_
